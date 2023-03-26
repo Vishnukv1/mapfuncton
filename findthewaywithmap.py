ea8bc42b08038dd9b880756accdd0b23dd6dd27f
Vishnukv1 <127046800+Vishnukv1@users.noreply.github.com>
2023-03-27 00:10:09 +0530

    Update findthewaywithmap.py

--- findthewaywithmap.py
+++ findthewaywithmap.py
@@ -1,5 +1,5 @@
 
-a =  input("enter the list of numbers :", ).split(',')
+a =  input("enter the list of numbers seperating by comma :", ).split(',')
 print(a)
 result = map(lambda x:int(x)*3,a)
 print(list(result))
